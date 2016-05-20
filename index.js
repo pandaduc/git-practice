9edc4d6fa628c026d883796296bcc4c19b5c65e5
Duc Anh Nguyen <duc.nguyen@elanmediapartners.com>
2016-05-21 08:15:46 +1000

    Behind 1 commit

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 
     var body = document.body;
 
+    run();
+    gun();
+
     function run() {
         body.innerHTML = 'Hello git';
+    }
+
+    function gun() {
+        console.log('putting something here');
     }
 
 })();
